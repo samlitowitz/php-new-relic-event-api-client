027c9c37ff6fe35733c852a85f69091b2d6a9f44
Sam L <samlitowitz@users.noreply.github.com>
2022-08-12 08:37:19 -0400

    Update function signature to return wrapped response

--- src/EventAPI/Client.php
+++ src/EventAPI/Client.php
@@ -3,7 +3,8 @@
 namespace PhpNewRelic\EventAPI;
 
 use PhpNewRelic\CustomEventCollection;
+use PhpNewRelic\EventAPI\Http\Response;
 
 interface Client {
-	public function send(CustomEventCollection $customEvents): void;
+	public function send(CustomEventCollection $customEvents): Response;
 }
